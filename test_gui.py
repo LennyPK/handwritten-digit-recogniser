6b5068b5dd5235c750d9a259ac63421a178bc5b7
Cynthia Kwok <ckwo844@aucklanduni.ac.nz>
2021-04-06 19:17:28 +1200

    test_gui_03
    Added 'view' menubar with test and train images.

--- test_gui.py
+++ test_gui.py
@@ -20,16 +20,32 @@ class MyApp(QMainWindow):
         quitAction.setStatusTip('Quit application')
         quitAction.triggered.connect(qApp.quit)
 
-        trainModel = QAction('Train Model', self)
-        #trainModel.setShortcut()
-        trainModel.setStatusTip('Train Model')
+        trainAction = QAction('Train Model', self)
+        #trainAction.setShortcut()
+        trainAction.setStatusTip('Train Model')
+        #trainAction.triggered.connect()
+
+        viewTrainAction = QAction('View Training Images', self)
+        #viewTrainAction.setShortcut()
+        #viewTrainAction.setStatusTip()
+        #viewTrainAction.triggered.connect()
+
+
+        viewTestAction = QAction('View Testing Images', self)
+        #viewTestAction.setShortcut()
+        #viewTestAction.setStatusTip()
+        #viewTestAction.triggered.connect()
+
 
         menubar = self.menuBar()
         #File menubar (train model, quit)
         filemenu = menubar.addMenu('&File')
         filemenu.addAction(quitAction)
-        filemenu.addAction(trainModel)
-        
+        filemenu.addAction(trainAction)
+
+        viewmenu = menubar.addMenu('&View')
+        viewmenu.addAction(viewTrainAction)        
+        viewmenu.addAction(viewTestAction)
 
 if __name__ == '__main__':
    app = QApplication(sys.argv)
